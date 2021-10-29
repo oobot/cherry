19e6e4ce85794eaea65e255b22aad8c64b28ee76
bugslabx <bugslabx@gmail.com>
2021-10-29 15:30:48 +0800

    Insert or update.

--- cherry/src/insert/mod.rs
+++ cherry/src/insert/mod.rs
@@ -11,21 +11,24 @@ use crate::tx::Transaction;
 
 pub struct Insert<'a> {
     ds: TypeId,
-    sql: Option<SqlBuilder>,
+    sql_builder: Option<SqlBuilder>,
     replace: Option<(String, String)>,
+    sql: Option<String>,
     tx: Option<&'a mut Transaction<'a>>,
     tx_auto: bool,
     arguments: Arguments<'a>,
-
 }
 
-#[allow(dead_code)]
+// #[allow(dead_code)]
 impl<'a> Insert<'a> {
     fn new(ds: TypeId) -> Self {
-        Self {ds, sql: None, replace: None, tx: None, tx_auto: false, arguments: Arguments::new()}
+        Self {
+            ds, sql_builder: None, replace: None, sql: None, tx: None, tx_auto: false,
+            arguments: Arguments::new()
+        }
     }
 
-    fn bulk_sql<T>(size: usize) -> SqlBuilder where T: Cherry {
+    fn sql_builder<T>(size: usize) -> SqlBuilder where T: Cherry {
         let columns = T::columns();
         let holders = vec!["?"; columns.len()];
         let mut sql = SqlBuilder::insert_into(T::table());
@@ -38,42 +41,62 @@ impl<'a> Insert<'a> {
 
     pub(crate) fn insert<T>(ds: TypeId, v: &'a T) -> Self where T: Cherry {
         let mut t = Self::new(ds);
-        t.sql = Some(Self::bulk_sql::<T>(1));
+        t.sql_builder = Some(Self::sql_builder::<T>(1));
         v.arguments(&mut t.arguments);
         t
     }
 
     pub(crate) fn insert_bulk<T>(ds: TypeId, v: &'a [T]) -> Self where T: Cherry {
         let mut t = Self::new(ds);
-        t.sql = Some(Self::bulk_sql::<T>(v.len()));
+        t.sql_builder = Some(Self::sql_builder::<T>(v.len()));
         v.iter().for_each(|v| v.arguments(&mut t.arguments) );
         t
     }
 
-    pub(crate) fn insert_ignore<T>(ds: TypeId, v: &'a [T]) -> Self where T: Cherry {
+    pub(crate) fn insert_or_ignore<T>(ds: TypeId, v: &'a [T]) -> Self where T: Cherry {
         let mut t = Self::new(ds);
-        t.sql = Some(Self::bulk_sql::<T>(v.len()));
+        t.sql_builder = Some(Self::sql_builder::<T>(v.len()));
         t.replace = Some(("INSERT".into(), "INSERT IGNORE".into()));
         v.iter().for_each(|v| v.arguments(&mut t.arguments) );
         t
     }
 
-    pub(crate) fn insert_replace<T>(ds: TypeId, v: &'a [T]) -> Self where T: Cherry {
+    pub(crate) fn insert_or_replace<T>(ds: TypeId, v: &'a [T]) -> Self where T: Cherry {
         let mut t = Self::new(ds);
-        t.sql = Some(Self::bulk_sql::<T>(v.len()));
+        t.sql_builder = Some(Self::sql_builder::<T>(v.len()));
         t.replace = Some(("INSERT INTO".into(), "INSERT REPLACE INTO".into()));
         v.iter().for_each(|v| v.arguments(&mut t.arguments) );
         t
     }
 
     pub fn tx(mut self, tx: &'a mut Transaction<'a>) -> Self {
+        self.tx_ref(tx);
+        self
+    }
+
+    fn tx_ref(&mut self, tx: &'a mut Transaction<'a>) -> &Self {
         self.tx = Some(tx);
         self
     }
 
     pub fn tx_auto(mut self) -> Self {
+        self.tx_auto_ref();
+        self
+    }
+
+    fn tx_auto_ref(&mut self) -> &Self {
         self.tx_auto = true;
         self
+    }
+
+    fn build_sql(&self) -> Result<String> {
+        let mut sql = self.sql_builder.as_ref()
+            .ok_or_else(|| anyhow!("SqlBuilder is empty. This wasn’t supposed to happen."))?
+            .sql()?;
+        if let Some((src, target)) = &self.replace {
+            sql = sql.replacen(src.as_str(), target.as_str(), 1);
+        }
+        Ok(sql)
     }
 
     pub async fn execute(self) -> Result<QueryResult>  {
@@ -81,24 +104,22 @@ impl<'a> Insert<'a> {
             return Err(anyhow!("Manual and automatic transactions can not exist simultaneously."));
         }
 
+        let sql = match self.sql {
+            Some(sql) => sql,
+            _ => self.build_sql()?,
+        };
+        let sql = sql.as_str();
         let arguments = self.arguments.inner;
-        let mut sql = self.sql.expect("Should not be happened.").sql()?;
-        if let Some((src, target)) = self.replace {
-            sql = sql.replacen(src.as_str(), target.as_str(), 1);
-        }
 
         let result = if let Some(tx) = self.tx { // Manual transaction.
-            sqlx::query_with(sql.as_str(), arguments).execute(&mut tx.inner).await?
+            sqlx::query_with(sql, arguments).execute(&mut tx.inner).await?
         } else if self.tx_auto { // Auto transaction.
             let mut tx = pool::get(self.ds)?.begin().await?;
-            let result = sqlx::query_with(sql.as_str(), arguments)
-                .execute(&mut tx)
-                .await?;
+            let result = sqlx::query_with(sql, arguments).execute(&mut tx).await?;
             tx.commit().await?;
             result
         } else { // No transaction.
-            let pool = pool::get(self.ds)?;
-            sqlx::query_with(sql.as_str(), arguments).execute(pool).await?
+            sqlx::query_with(sql, arguments).execute(pool::get(self.ds)?).await?
         };
 
         Ok(QueryResult::from(result))
@@ -106,8 +127,71 @@ impl<'a> Insert<'a> {
 
 }
 
+pub struct InsertOrUpdate<'a> {
+    insert: Insert<'a>,
+    fields: Vec<String>,
+}
 
+impl<'a> InsertOrUpdate<'a> {
 
-pub struct InsertUpdate {
-    // Include Insert
+    fn new(ds: TypeId) -> Self {
+        Self { insert: Insert::new(ds), fields: vec![] }
+    }
+
+    pub(crate) fn insert_or_update<T>(ds: TypeId, v: &'a [T]) -> Self where T: Cherry {
+        let mut t = Self::new(ds);
+        t.insert.sql_builder = Some(Insert::sql_builder::<T>(v.len()));
+        v.iter().for_each(|v| v.arguments(&mut t.insert.arguments) );
+        t
+    }
+
+    pub fn field<T: AsRef<str>>(mut self, f: T) -> Self {
+        self.field_ref(f);
+        self
+    }
+
+    pub fn field_ref<T: AsRef<str>>(&mut self, f: T) -> &Self {
+        self.fields.push(f.as_ref().to_owned());
+        self
+    }
+
+    pub fn fields<T: AsRef<str>>(mut self, f: &[T]) -> Self {
+        self.fields_ref(f);
+        self
+    }
+
+    pub fn fields_ref<T: AsRef<str>>(&mut self, f: &[T]) -> &Self {
+        f.iter().for_each(|f| {
+            self.fields.push(f.as_ref().to_owned());
+        });
+        self
+    }
+
+    pub fn tx(mut self, tx: &'a mut Transaction<'a>) -> Self {
+        self.insert.tx_ref(tx);
+        self
+    }
+
+    pub fn tx_auto(mut self) -> Self {
+        self.insert.tx_auto_ref();
+        self
+    }
+
+    pub async fn execute(mut self) -> Result<QueryResult>  {
+        if self.fields.is_empty() {
+            return Err(anyhow!("Empty update fields."));
+        }
+        let insert = self.insert.build_sql()?.strip_suffix(";")
+            .ok_or(anyhow!("Empty sql. This wasn’t supposed to happen."))?
+            .to_owned();
+        let update = self.fields.iter().map(|x| format!("{0} = new.{0}, ", x))
+            .collect::<String>()
+            .strip_suffix(",")
+            .ok_or(anyhow!("Empty sql. This wasn’t supposed to happen."))?
+            .to_owned();
+
+        self.insert.sql = Some(format!("{} AS new ON DUPLICATE KEY UPDATE {};", insert, update));
+        self.insert.execute().await
+    }
+
 }
