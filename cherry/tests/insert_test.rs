8de3e5141e25bf86be6298a4b83095413c4e5fc9
bitlabx <bitweng@gmail.com>
2022-12-17 12:58:07 +0800

    tests

--- cherry/tests/insert_test.rs
+++ cherry/tests/insert_test.rs
@@ -1,7 +1,8 @@
-use sqlx::Executor;
+use sqlx::{Executor, Row};
 
 use cherry::pool::sqlite::SqlitePool;
 use cherry::query::insert::insert::Insert;
+use cherry::sqlx::types::Json;
 use cherry_derive::Cherry;
 
 async fn init() -> SqlitePool {
@@ -18,11 +19,40 @@ async fn test_insert_one() {
         .execute(&pool.inner).await.unwrap();
     assert_eq!(1, r.rows_affected());
 
+    let sql = "select * from user where id = 100";
+    let row = sqlx::query(sql).fetch_one(&pool.inner).await.unwrap();
+    let result = User { id: row.get("id"), name: row.get("name"), age: row.get("age"), };
+    assert_eq!(&user, &result);
 }
 
-#[derive(Cherry)]
+#[async_std::test]
+async fn test_insert_ignore() {
+
+}
+
+#[async_std::test]
+async fn test_insert_update() {
+
+}
+
+#[async_std::test]
+async fn test_insert_replace() {
+
+}
+
+
+#[derive(Debug, Cherry, Eq, PartialEq)]
 struct User {
     id: u32,
     name: String,
     age: u8,
+}
+
+#[derive(Debug, Cherry, Eq, PartialEq)]
+struct Book {
+    id: u32,
+    name: String,
+    authors: Json<Vec<String>>,
+    edition: u8,
+    published_date:
 }
--- cherry/tests/migrations.sql
+++ cherry/tests/migrations.sql
@@ -5,3 +5,13 @@ create table user (
     `age` int unsigned not null,
     primary key (`id`)
 ) without rowid;
+
+drop table if exists book;
+create table book (
+    `id` int unsigned not null,
+    `name` text not null,
+    `authors` json not null,
+    `edition` int unsigned not null,
+    `published_date` text not null,
+    primary key (`id`)
+) without rowid;
